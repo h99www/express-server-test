cb077dae197678d1312802141f2a855fc7377c9c
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-07-25 01:23:46 +0900

    no message

---
+++ src/routes/challenge-route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const ChallengeController = require('../controllers/challenge/challenge-controller');
+
+router.get('/', ChallengeController.findChallenges);
+router.post('/', ChallengeController.registChallenge);
+router.put('/',ChallengeController.modifyChallenge)
+router.delete('/',ChallengeController.removeChallenge)
+router.post('/upload', ChallengeController.uploadFile);
+router.get('/rankings', ChallengeController.findRankings);
+router.get('/checkAuth', ChallengeController.checkChallengeAuthByMemberNo);
+router.put('/secession', ChallengeController.secessionChallenge);
+router.get('/category/:categoryNo', ChallengeController.findByCategoryNo);
+router.get('/:challengeNo', ChallengeController.findChallengeByNo);
+router.post('/:challengeNo', ChallengeController.participateChallenge);
+module.exports = router;
